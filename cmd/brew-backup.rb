4680266519cbb6026d024998332714ece3f234a6
Shyam Sunder <sgsunder1@gmail.com>
2018-09-01 19:31:03 -0400

    Fixed backup script

--- cmd/brew-backup.rb
+++ cmd/brew-backup.rb
@@ -1,26 +1,30 @@
-backup_path = Dir.home() + "/.homebrew-backup"
-brewfile = backup_path + "/Brewfile.conf"
+# Some constants to customize
+bf_directory = ".homebrew-backup"
+bf_basename = "Brewfile.conf"
 
-if File.directory?(backup_path)
-    ohai "Backup using path #{backup_path}"
+
+if File.directory?(Dir.home() + "/" + bf_directory)
+    ohai "Backup using path ~/#{bf_directory}"
 else
     #TODO : Interactively create the directory
     odie "Directory does not exist!"
 end
 
 ohai "Listing all installed Homebrew packages"
-if File.file?(brewfile)
-    File.delete(brewfile)
+bf_fullpath = Dir.home() + "/" + bf_directory + "/" + bf_basename
+if File.file?(bf_fullpath)
+    File.delete(bf_fullpath)
 end
-system "brew", "bundle", "dump", "--file=#{brewfile}"
+system "brew", "bundle", "dump", "--file=#{bf_fullpath}"
 
-Dir.chdir(backup_path) do
-    if !system "git", "diff", "--quiet"
-        ohai "Committing changes..."
-        system "git", "add", "."
-        system "git", "commit", "--all", "--allow-empty-message", "-m", ""
-        system "git", "push", "origin", "master"
-    else
+Dir.chdir(Dir.home() + "/" + bf_directory) do
+    system "git", "diff", "--quiet" # stupid hack to make the next line work
+    if system "git", "diff-files", "--quiet"
         ohai "No changes detected"
+    else
+        ohai "Committing changes..."
+        system "git", "add", "#{bf_basename}"
+        system "git", "commit", "--quiet", "--allow-empty-message", "-m", ""
+        system "git", "push", "--porcelain", "origin", "master"
     end
 end
