e3bd9bab92d0342419a0eadde377bcf9a113358b
Shyam Sunder <sgsunder1@gmail.com>
2020-01-05 00:06:07 -0500

    Fix Videoconvert repo URL

--- Formula/videoconvert.rb
+++ Formula/videoconvert.rb
@@ -1,7 +1,7 @@
 class Videoconvert < Formula
   desc "Helper tool for quick ffmpeg conversions"
   homepage "https://gist.github.com/sgsunder/2c099dedee6172cf1ed93ca5d310821d"
-  url "https://gist.github.com/feecbbff7e1641bb95a4a916560f9a52.git"
+  url "https://gist.github.com/2c099dedee6172cf1ed93ca5d310821d.git"
   version "latest"
 
   def install
