eefcda385ed32a7def95ebc9b3b359f7c7ade808
Shyam Sunder <sgsunder1@gmail.com>
2020-07-21 19:24:41 -0400

    Update waifu2x as head-only

--- Formula/waifu2x-converter-cpp.rb
+++ Formula/waifu2x-converter-cpp.rb
@@ -1,8 +1,6 @@
 class Waifu2xConverterCpp < Formula
   desc "Improved fork of Waifu2X C++ using OpenCL and OpenCV"
   homepage "https://github.com/DeadSix27/waifu2x-converter-cpp"
-  url "https://github.com/DeadSix27/waifu2x-converter-cpp/archive/v5.3.3.tar.gz"
-  sha256 "036d82bb4ec2e4a098b084e5f82f21d3a274c6c16e60e7d5dd44478f2cb463ed"
   head "https://github.com/DeadSix27/waifu2x-converter-cpp.git"
 
   depends_on "cmake" => :build
